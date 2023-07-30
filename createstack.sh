63d9a7c999487a7b18d2223f6ff650e24f906d18
Veerabathiran R <veeravee93@gmail.com>
2023-07-30 17:14:30 +0530

    Add files via upload

---
+++ createstack.sh
@@ -0,0 +1,7 @@
+aws cloudformation create-stack \
+--stack-name $1 \
+--template-body file://D:/Udacity/Project/$2 \
+--parameters file://D:/Udacity/Project/$3 \
+--capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM" \
+--region=us-west-2 \
+--profile default
